233c9f25a77c31e1cb093e75f5a86b4e0e7cb573
unknown <xyndergaming@gmail.com>
2018-01-17 19:46:22 +0200

    Array exercises

---
+++ Arrays/Exercise 4/Exercise 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =
+            {
+                2, 1, 1, 2, 3, 2, 2, 2, 2, 1
+            };
+            int start = 0;
+            int bestStart = 0;
+            int lenght = 1;
+            int bestLenght = 0;
+
+            for (int i = 0; i < array.Length - 1 ; i++)
+            {
+                start = array[i];
+                if(array[i] == array[i + 1])
+                {
+                    lenght++;
+                }
+                if (array[i] != array[i + 1])
+                {
+                    lenght = 1;
+                }
+                if (lenght > bestLenght)
+                {
+                    bestLenght = lenght;
+                    bestStart = start;
+                }
+            }
+            for (int i = 0; i < bestLenght; i++)
+            {
+                Console.Write(bestStart + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Arrays/Exercise 5/Exercise 5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =
+            {
+                3, 2, 3, 4, 2, 2, 4, 1, 2, 3, 4, 5
+            };
+            int start = 0;
+            int bestStart = 0;
+            int lenght = 1;
+            int bestLenght = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                start = array[i];
+                if (array[i] == array[i + 1] - 1)
+                {
+                    lenght++;
+                }
+                if (array[i] != array[i + 1] - 1)
+                {
+                    lenght = 1;
+                }
+                if (lenght > bestLenght)
+                {
+                    bestLenght = lenght;
+                    bestStart = start;
+                }
+            }
+            for (int i = bestStart-(bestLenght-2); i <= bestStart + 1; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
